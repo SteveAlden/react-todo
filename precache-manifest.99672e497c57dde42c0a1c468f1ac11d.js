21d0925d1521c0514b80096e6b7edb13183bb9af
Steve Alden <stevealden.77@gmail.com>
2019-12-19 18:12:59 +0530

    Updates

--- precache-manifest.99672e497c57dde42c0a1c468f1ac11d.js
+++ precache-manifest.99672e497c57dde42c0a1c468f1ac11d.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b68d1947548026a9ace801de87caf5d5",
+    "revision": "0ca34be5d469aa763b48cbc551cd4158",
     "url": "/ReactTodo/index.html"
   },
   {
-    "revision": "d600ce4bcbda196e468c",
+    "revision": "3b63400762ea4647612a",
     "url": "/ReactTodo/static/css/main.b6b6a3b1.chunk.css"
   },
   {
-    "revision": "8ee9f435c3867acdf2a5",
-    "url": "/ReactTodo/static/js/2.814fb77f.chunk.js"
+    "revision": "81db264e149a8543a8c6",
+    "url": "/ReactTodo/static/js/2.327d80a8.chunk.js"
   },
   {
     "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
-    "url": "/ReactTodo/static/js/2.814fb77f.chunk.js.LICENSE"
+    "url": "/ReactTodo/static/js/2.327d80a8.chunk.js.LICENSE"
   },
   {
-    "revision": "d600ce4bcbda196e468c",
-    "url": "/ReactTodo/static/js/main.b78692c0.chunk.js"
+    "revision": "3b63400762ea4647612a",
+    "url": "/ReactTodo/static/js/main.40f2dc36.chunk.js"
   },
   {
     "revision": "b3206e9cd40ae2e1a1a6",
