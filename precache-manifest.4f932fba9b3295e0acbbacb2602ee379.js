c6f39e21684cd945892a235412a3c85d461f08f6
Steve Alden <stevealden.77@gmail.com>
2019-12-19 14:40:25 +0530

    Updates

---
+++ precache-manifest.4f932fba9b3295e0acbbacb2602ee379.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd850604c3eafb83a0953276430cb88e",
+    "url": "/ReactTodo/index.html"
+  },
+  {
+    "revision": "984c67415f62554ce339",
+    "url": "/ReactTodo/static/css/main.b6b6a3b1.chunk.css"
+  },
+  {
+    "revision": "8ee9f435c3867acdf2a5",
+    "url": "/ReactTodo/static/js/2.814fb77f.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/ReactTodo/static/js/2.814fb77f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "984c67415f62554ce339",
+    "url": "/ReactTodo/static/js/main.8158366d.chunk.js"
+  },
+  {
+    "revision": "b3206e9cd40ae2e1a1a6",
+    "url": "/ReactTodo/static/js/runtime-main.cd68fe30.js"
+  }
+]);
