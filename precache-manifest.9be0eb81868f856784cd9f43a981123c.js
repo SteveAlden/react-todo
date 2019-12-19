e540d8feaa0e944b9f89637318612c5ed5f9946b
Steve Alden <stevealden.77@gmail.com>
2019-12-19 18:17:24 +0530

    Updates

--- precache-manifest.9be0eb81868f856784cd9f43a981123c.js
+++ precache-manifest.9be0eb81868f856784cd9f43a981123c.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0ca34be5d469aa763b48cbc551cd4158",
+    "revision": "264d8ee47d1fe185e1fb397902de0051",
     "url": "/ReactTodo/index.html"
   },
   {
-    "revision": "3b63400762ea4647612a",
+    "revision": "aff896c9ecdbdb67e4ba",
     "url": "/ReactTodo/static/css/main.b6b6a3b1.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ReactTodo/static/js/2.327d80a8.chunk.js.LICENSE"
   },
   {
-    "revision": "3b63400762ea4647612a",
-    "url": "/ReactTodo/static/js/main.40f2dc36.chunk.js"
+    "revision": "aff896c9ecdbdb67e4ba",
+    "url": "/ReactTodo/static/js/main.95a6b23d.chunk.js"
   },
   {
     "revision": "b3206e9cd40ae2e1a1a6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ReactTodo/precache-manifest.99672e497c57dde42c0a1c468f1ac11d.js"
+  "/ReactTodo/precache-manifest.9be0eb81868f856784cd9f43a981123c.js"
 );
 
 self.addEventListener('message', (event) => {
